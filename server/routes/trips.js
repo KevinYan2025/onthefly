67825690b71db766f8bed4b605a25e375ea19527
kevinyan2025 <yan767957598@gmail.com>
2024-10-24 17:25:25 -0400

    complete part 2

--- server/routes/trips.js
+++ server/routes/trips.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import TripsController from '../controllers/trips.js'
+import {getTrip,createTrip,deleteTrip,updateTrip} from '../controllers/trips.js'
 
 const router = express.Router()
 
-router.get('/', TripsController.getTrips)
-router.get('/:id', TripsController.getTrip)
-router.post('/', TripsController.createTrip)
-router.delete('/:id', TripsController.deleteTrip)
-router.patch('/:id', TripsController.updateTrip)
+router.get('/', getTrip)
+router.get('/:id', getTrip)
+router.post('/', createTrip)
+router.delete('/:id', deleteTrip)
+router.patch('/:id', updateTrip)
 
 export default router
